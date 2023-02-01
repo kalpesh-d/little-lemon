1f9d550e6133bc5705a80e12b969cf6ad8ca2feb
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-02-01 19:06:10 +0530

    defined basic footer structure & added logo for it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,35 @@
 function Footer() {
     return (
-        <footer></footer>
+        <footer>
+            <img src={require('../assets/footer logo.png')} alt="Little Lemon" />
+
+            {/* Doormat Navigation */}
+            <ul>
+                <h4>Doormat Navigation</h4>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+
+            {/* Contact */}
+            <ul>
+                <h4>Contact</h4>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+
+            {/* Social Media */}
+            <ul>
+                <h4>Social Media Links</h4>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+        </footer>
     );
 }
 
