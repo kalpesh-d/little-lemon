5ca54c0328635835779cdb244f573ebd25892a29
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-02-01 18:11:22 +0530

    added semantic tag that will be react components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navigation />
+      <Main>
+        <Special/>
+        <Testimonial />
+        <About />
+      </Main>
+      <Footer />
+    </>
   );
 }
 
