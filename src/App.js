7b30161da121f2e86c6fe3768c6bafa710db88e5
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-02-01 18:46:21 +0530

    imported all components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import Navigation from './components/Navigation';
+import Main from './components/Main';
+import Special from './components/Special';
+import Testimonial from './components/Testimonial';
+import About from './components/About';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
