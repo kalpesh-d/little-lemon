dd7f4a3a5c3531c44fa66302df6cc58ef1d63609
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-02-02 15:51:17 +0530

    made navigation responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,12 +6,12 @@ function Navigation() {
         <nav className='navigation'>
             <img src={Logo} alt="Little Lemon" className='logo'/>
             <ul className='nav-lists'>
-                <li className='list'><a href="">Home</a></li>
-                <li className='list'><a href="">About</a></li>
-                <li className='list'><a href="">Menu</a></li>
-                <li className='list'><a href="">Reservation</a></li>
-                <li className='list'><a href="">Order Online</a></li>
-                <li className='list'><a href="">Login</a></li>
+                <li className='list'><a href="/">Home</a></li>
+                <li className='list'><a href="/">About</a></li>
+                <li className='list'><a href="/">Menu</a></li>
+                <li className='list'><a href="/">Reservation</a></li>
+                <li className='list'><a href="/">Order Online</a></li>
+                <li className='list'><a href="/">Login</a></li>
             </ul>
         </nav>
     );
