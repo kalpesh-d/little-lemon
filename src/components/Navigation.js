55d12ab4598b4dc894563439530d9f9413735317
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-02-02 12:41:23 +0530

    added basic styles in index

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import Logo from '../assets/Logo.svg';
 function Navigation() {
     return(
         <nav className='navigation'>
-            <img src={Logo} alt="Little Lemon" className='primary-logo'/>
+            <img src={Logo} alt="Little Lemon" className='logo'/>
             <ul className='nav-lists'>
                 <li className='list'><a href="">Home</a></li>
                 <li className='list'><a href="">About</a></li>
