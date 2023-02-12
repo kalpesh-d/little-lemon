d38531dc060890c0d2c7842f328ffa8286111d75
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-02-12 23:29:05 +0530

    done

--- src/components/form.test.js
+++ src/components/form.test.js
@@ -8,26 +8,26 @@ describe ('Check the name format', () => {
     test('render name input', () => {
     render(<BookingForm />);
  
-    const test2 = screen.getByTestId("name-input");
+    const test2 = screen.getByTestId("res-name");
     expect(test2).toBeInTheDocument();
     expect(test2).toHaveAttribute("type", "name");
   });
     test("If the name is correct", () => {
         render(<BookingForm />);
 
-        const test2 = screen.getByTestId("name-input");
+        const test2 = screen.getByTestId("res-name");
         userEvent.type(test2, "John");
 
-    expect(screen.getByTestId("name-input")).toHaveValue("John");
+    expect(screen.getByTestId("res-name")).toHaveValue("John");
     expect(screen.queryByTestId("error-msg")).not.toBeInTheDocument();
     });
     test('pass invalid name to test input value', () => {
     render(<BookingForm />);
  
-    const test2 = screen.getByTestId("name-input");
+    const test2 = screen.getByTestId("res-name");
     userEvent.type(test2, "34ygs");
  
-    expect(screen.getByTestId("name-input")).toHaveValue("34ygs");
+    expect(screen.getByTestId("res-name")).toHaveValue("34ygs");
     expect(screen.queryByTestId("error-msg").textContent).toEqual("Please enter a valid name.");
   });
 })
@@ -37,7 +37,7 @@ describe('Check the date format', () => {
     test('render date input', () => {
     render(<BookingForm />);
  
-    const test1 = screen.getByTestId("date-input");
+    const test1 = screen.getByTestId("res-date");
     expect(test1).toBeInTheDocument();
     expect(test1).toHaveAttribute("type", "date");
   });
@@ -45,20 +45,20 @@ describe('Check the date format', () => {
     test("If the date is incorrect", () =>{
         render(<BookingForm />);
 
-        const test1 = screen.getByTestId("date-input");
+        const test1 = screen.getByTestId("res-date");
         userEvent.type(test1, "02/10/2023");
 
-    expect(screen.getByTestId("date-input")).toHaveValue("02/10/2023");
+    expect(screen.getByTestId("res-date")).toHaveValue("02/10/2023");
     expect(screen.queryByTestId("error-msg")).not.toBeInTheDocument();
     });
 
     test('pass invalid date to test input value', () => {
     render(<BookingForm />);
  
-    const test1 = screen.getByTestId("date-input");
+    const test1 = screen.getByTestId("res-date");
     userEvent.type(test1, "20/02/2022");
  
-    expect(screen.getByTestId("date-input")).toHaveValue("20/02/2022");
+    expect(screen.getByTestId("res-date")).toHaveValue("20/02/2022");
     expect(screen.queryByTestId("error-msg")).toBeInTheDocument();
     expect(screen.queryByTestId("error-msg").textContent).toEqual("Please enter a valid date.");
   });
